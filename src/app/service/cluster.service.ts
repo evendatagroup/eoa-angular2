fbcb0f5def40d07842cf7fdf457b6c3ab05d4c3e
zhengnan <zhengn@evendatagroup.com>
2018-01-18 10:30:03 +0800

    载信通群信息

--- src/app/service/cluster.service.ts
+++ src/app/service/cluster.service.ts
@@ -9,11 +9,30 @@ export class ClusterService {
     constructor(private http: _HttpClient,private http2: HttpClient) { }
 
     // 获取公司所有人（通讯录）
-    getLisWithUser(): Promise<Cluster[]> {
+    getLisWithUser(): Promise<any[]> {
         return new Promise((resolve, reject) => {
             this.http.get('oaCluster/getListWithUser')
                 .subscribe((res: any) => {
-                    resolve(res.data)
+                  let result = [];
+                  result = res.data.reduce((previous, current) => {
+                    let findcurrent = false;
+                    for(let i=0;i<previous.length;i++){
+                      if(previous[i].clusterVid==current.clusterVid){
+                        findcurrent = true;
+                        previous[i].children.push(current)
+                      }
+                    }
+                    if(!findcurrent) {
+                      let c = {}
+                      for(let key in current){
+                        c[key]=current[key];
+                      }
+                      current.children = [c];
+                      previous.push(current);
+                    }
+                    return previous
+                  }, [])
+                    resolve(result)
                 })
         })
     }
@@ -23,7 +42,7 @@ export class ClusterService {
             this.http.get('oaCluster/getList', { clusterVid })
                 .subscribe((res: any) => {
                     resolve(res.data[0])
-                }) 
+                })
         })
     }
 }
