7ff40db26829ff9d5adbc73890d15722fd7221d5
zhengnan <zhengn@evendatagroup.com>
2018-01-31 09:26:07 +0800

    no message

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     chore: false,
-    SERVER_URL: `./`,
+    SERVER_URL: `http://192.168.0.10:3001`,
     HTTP_URL: `http://192.168.0.10/eoa/`,
     FILE_URL: `http://192.168.0.10/eoa/file/`,
     production: true,
